f18ad70af1387a106dbd9e3fbf786d71a29358f1
Zefile8 <85622063+Zefile8@users.noreply.github.com>
2022-04-23 18:47:23 +0200

    added default output file

---
+++ Output.lua
@@ -0,0 +1,15 @@
+--<Card Name>
+--Edo Autoscript written by Zefile#5500
+local s,id = GetID()
+function s.initial_effect(c)
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(<edit settype>)
+	e1:SetCode(<edit setcode>)
+	<expand effect>
+	c:RegisterEffect(e1)
+end
+<add condition>
+<add target>
+<add operation>
+<add func>
+--this is the default file Autoscript will produce if given no instructions.
